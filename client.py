9d0898d62d6099b789b9fc78b64c9a98d194dc64
elfremix <44305315+elfremix@users.noreply.github.com>
2018-11-16 17:32:02 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,2 +1,33 @@
 import argparse
 import socket
+import smtplib
+from email.mime.text import MIMEText
+
+#action de depart.
+x = input('1. se connecter \n'
+          '2. Creer un compte')
+
+if x == 1:
+    #...
+
+else:
+    #...
+
+#envoie courriel.
+
+rcptto = input('Rcpt to : ')
+subject = input('Subject')
+
+print('Data : ')
+text = ""
+temp = input()
+while temp != ".":
+    text += temp + "\n"
+    temp = input()
+
+msg = MIMEText(text)
+#msg["From"] = client.adresse
+msg['To'] = rcptto
+msg['Subject'] = subject
+
+
